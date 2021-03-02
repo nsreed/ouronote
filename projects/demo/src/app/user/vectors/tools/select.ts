fb4de24085c4aae9ac3822c699a60107db8791c8
Norman Reed <norman.s.reed@gmail.com>
2021-03-01 23:02:10 -0500

    select tool work

--- projects/demo/src/app/user/vectors/tools/select.ts
+++ projects/demo/src/app/user/vectors/tools/select.ts
@@ -10,15 +10,28 @@ export class SelectTool extends VectorTool {
         (this.scope.settings as any).insertItems = false;
         this.path = this.path || new paper.Path([e.downPoint]);
         this.path.data.ignored = true;
+        this.path.dashArray = [12, 12];
+        this.path.closed = true;
+        this.path.fillColor = new paper.Color(0, 0, 1, 0.5) as any;
         (this.scope.settings as any).insertItems = prev;
-
         this.project.activeLayer.insertChild(0, this.path);
       }
       this.path.add(e.point);
     });
     this.up.subscribe((e) => {
-      this.path.remove();
-      this.path = null as never;
+      if (this.path) {
+        const intersected = this.scope.project.getItems({
+          match: (item: paper.Item) =>
+            item.className !== 'Layer' && this.path?.intersects(item),
+        });
+        this.scope.project.deselectAll();
+        intersected.forEach((i) => (i.selected = true));
+
+        this.path.remove();
+        this.path = null as never;
+      } else {
+        this.scope.project.deselectAll();
+      }
     });
   }
 }
