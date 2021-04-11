d4852ade49bcaf26815be41d4390037cbbacb570
Norman Reed <norman.s.reed@gmail.com>
2021-04-11 03:42:12 -0400

    more builds

--- dist/demo/polyfills.js
+++ dist/demo/polyfills.js
@@ -28284,9 +28284,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var gun_lib_not__WEBPACK_IMPORTED_MODULE_11___default = /*#__PURE__*/__webpack_require__.n(gun_lib_not__WEBPACK_IMPORTED_MODULE_11__);
 /* harmony import */ var gun_lib_unset__WEBPACK_IMPORTED_MODULE_12__ = __webpack_require__(/*! gun/lib/unset */ "8Jsz");
 /* harmony import */ var gun_lib_unset__WEBPACK_IMPORTED_MODULE_12___default = /*#__PURE__*/__webpack_require__.n(gun_lib_unset__WEBPACK_IMPORTED_MODULE_12__);
-/* harmony import */ var paper__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! paper */ "IiLU");
-/* harmony import */ var paper__WEBPACK_IMPORTED_MODULE_13___default = /*#__PURE__*/__webpack_require__.n(paper__WEBPACK_IMPORTED_MODULE_13__);
-/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! @angular/core */ "fXoL");
+/* harmony import */ var gun_lib_webrtc__WEBPACK_IMPORTED_MODULE_13__ = __webpack_require__(/*! gun/lib/webrtc */ "pmEQ");
+/* harmony import */ var gun_lib_webrtc__WEBPACK_IMPORTED_MODULE_13___default = /*#__PURE__*/__webpack_require__.n(gun_lib_webrtc__WEBPACK_IMPORTED_MODULE_13__);
+/* harmony import */ var paper__WEBPACK_IMPORTED_MODULE_14__ = __webpack_require__(/*! paper */ "IiLU");
+/* harmony import */ var paper__WEBPACK_IMPORTED_MODULE_14___default = /*#__PURE__*/__webpack_require__.n(paper__WEBPACK_IMPORTED_MODULE_14__);
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_15__ = __webpack_require__(/*! @angular/core */ "fXoL");
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -28360,7 +28362,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
-// import 'gun/lib/webrtc';
+
 /* PAPER.JS OVERRIDES
  * Injects rxjs-style property change observables into paper.js
  */
@@ -28424,7 +28426,7 @@ function addChangeEmitter(prototype) {
         Object.defineProperty(prototype, 'changes$', {
             get() {
                 if (!this._changes$) {
-                    this._changes$ = new _angular_core__WEBPACK_IMPORTED_MODULE_14__["EventEmitter"]();
+                    this._changes$ = new _angular_core__WEBPACK_IMPORTED_MODULE_15__["EventEmitter"]();
                 }
                 return this._changes$;
             },
@@ -28433,19 +28435,19 @@ function addChangeEmitter(prototype) {
     }
 }
 const toIntercept = [
-    paper__WEBPACK_IMPORTED_MODULE_13__["Item"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["Path"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["Layer"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["Shape"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["Shape"].Circle,
-    paper__WEBPACK_IMPORTED_MODULE_13__["Shape"].Ellipse,
-    paper__WEBPACK_IMPORTED_MODULE_13__["Shape"].Rectangle,
-    paper__WEBPACK_IMPORTED_MODULE_13__["Style"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["Group"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["Gradient"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["Color"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["View"],
-    paper__WEBPACK_IMPORTED_MODULE_13__["Size"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Item"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Path"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Layer"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Shape"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Shape"].Circle,
+    paper__WEBPACK_IMPORTED_MODULE_14__["Shape"].Ellipse,
+    paper__WEBPACK_IMPORTED_MODULE_14__["Shape"].Rectangle,
+    paper__WEBPACK_IMPORTED_MODULE_14__["Style"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Group"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Gradient"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Color"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["View"],
+    paper__WEBPACK_IMPORTED_MODULE_14__["Size"],
 ].map((con) => con.prototype);
 toIntercept.forEach((proto) => interceptAll(proto));
 function addGunProperty(prototype) {
@@ -74123,6 +74125,123 @@ ${errors.map((err, i) => `${i + 1}) ${err.toString()}`).join('\n  ')}` : '';
 })();
 const UnsubscriptionError = UnsubscriptionErrorImpl;
 //# sourceMappingURL=UnsubscriptionError.js.map
+
+/***/ }),
+
+/***/ "pmEQ":
+/*!********************************************!*\
+  !*** /home/nsreed/repos/gun/lib/webrtc.js ***!
+  \********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+;(function(){
+	var Gun = (typeof window !== "undefined")? window.Gun : __webpack_require__(/*! ../gun */ "mKbU");
+
+	Gun.on('opt', function(root){
+		this.to.next(root);
+		var opt = root.opt;
+		if(root.once){ return }
+		if(!Gun.Mesh){ return }
+		if(false === opt.RTCPeerConnection){ return }
+
+		var env;
+		if(typeof window !== "undefined"){ env = window }
+		if(typeof global !== "undefined"){ env = global }
+		env = env || {};
+
+		var rtcpc = opt.RTCPeerConnection || env.RTCPeerConnection || env.webkitRTCPeerConnection || env.mozRTCPeerConnection;
+		var rtcsd = opt.RTCSessionDescription || env.RTCSessionDescription || env.webkitRTCSessionDescription || env.mozRTCSessionDescription;
+		var rtcic = opt.RTCIceCandidate || env.RTCIceCandidate || env.webkitRTCIceCandidate || env.mozRTCIceCandidate;
+		if(!rtcpc || !rtcsd || !rtcic){ return }
+		opt.RTCPeerConnection = rtcpc;
+		opt.RTCSessionDescription = rtcsd;
+		opt.RTCIceCandidate = rtcic;
+		opt.rtc = opt.rtc || {'iceServers': [
+      {urls: 'stun:stun.l.google.com:19302'},
+      {urls: "stun:stun.sipgate.net:3478"}/*,
+      {urls: "stun:stun.stunprotocol.org"},
+      {urls: "stun:stun.sipgate.net:10000"},
+      {urls: "stun:217.10.68.152:10000"},
+      {urls: 'stun:stun.services.mozilla.com'}*/ 
+    ]};
+    // TODO: Select the most appropriate stuns. 
+    // FIXME: Find the wire throwing ICE Failed
+    // The above change corrects at least firefox RTC Peer handler where it **throws** on over 6 ice servers, and updates url: to urls: removing deprecation warning 
+    opt.rtc.dataChannel = opt.rtc.dataChannel || {ordered: false, maxRetransmits: 2};
+    opt.rtc.sdp = opt.rtc.sdp || {mandatory: {OfferToReceiveAudio: false, OfferToReceiveVideo: false}};
+    opt.announce = function(to){
+			root.on('out', {rtc: {id: opt.pid, to:to}}); // announce ourself
+    };
+		var mesh = opt.mesh = opt.mesh || Gun.Mesh(root);
+		root.on('create', function(at){
+			this.to.next(at);
+			setTimeout(opt.announce, 1);
+		});
+		root.on('in', function(msg){
+			if(msg.rtc){ open(msg) }
+			this.to.next(msg);
+		});
+
+		function open(msg){
+			var rtc = msg.rtc, peer, tmp;
+			if(!rtc || !rtc.id){ return }
+			delete opt.announce[rtc.id]; /// remove after connect
+			if(tmp = rtc.answer){
+				if(!(peer = opt.peers[rtc.id] || open[rtc.id]) || peer.remoteSet){ return }
+				return peer.setRemoteDescription(peer.remoteSet = new opt.RTCSessionDescription(tmp)); 
+			}
+			if(tmp = rtc.candidate){
+				peer = opt.peers[rtc.id] || open[rtc.id] || open({rtc: {id: rtc.id}});
+				return peer.addIceCandidate(new opt.RTCIceCandidate(tmp));
+			}
+			//if(opt.peers[rtc.id]){ return }
+			if(open[rtc.id]){ return }
+			(peer = new opt.RTCPeerConnection(opt.rtc)).id = rtc.id;
+			var wire = peer.wire = peer.createDataChannel('dc', opt.rtc.dataChannel);
+			open[rtc.id] = peer;
+			wire.onclose = function(){
+				delete open[rtc.id];
+				mesh.bye(peer);
+				//reconnect(peer);
+			};
+			wire.onerror = function(err){};
+			wire.onopen = function(e){
+				//delete open[rtc.id];
+				mesh.hi(peer);
+			}
+			wire.onmessage = function(msg){
+				if(!msg){ return }
+				mesh.hear(msg.data || msg, peer);
+			};
+			peer.onicecandidate = function(e){ // source: EasyRTC!
+        if(!e.candidate){ return }
+        root.on('out', {'@': msg['#'], rtc: {candidate: e.candidate, id: opt.pid}});
+			}
+			peer.ondatachannel = function(e){
+				var rc = e.channel;
+				rc.onmessage = wire.onmessage;
+				rc.onopen = wire.onopen;
+				rc.onclose = wire.onclose;
+			}
+			if(tmp = rtc.offer){
+				peer.setRemoteDescription(new opt.RTCSessionDescription(tmp)); 
+				peer.createAnswer(function(answer){
+					peer.setLocalDescription(answer);
+					root.on('out', {'@': msg['#'], rtc: {answer: answer, id: opt.pid}});
+				}, function(){}, opt.rtc.sdp);
+				return;
+			}
+			peer.createOffer(function(offer){
+				peer.setLocalDescription(offer);
+				root.on('out', {'@': msg['#'], rtc: {offer: offer, id: opt.pid}});
+			}, function(){}, opt.rtc.sdp);
+			return peer;
+		}
+	});
+	var noop = function(){};
+}());
+
 
 /***/ }),
 
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const path = require("path");
 const express = require("express");
 const Gun = require("gun/gun");
 const app = express();
+app.use(Gun.serve);
 app.use(express.static(__dirname + "/dist/demo"));
+
 app.get("/*", function (req, res) {
   // console.log({ req, res });
   res.sendFile(path.join(__dirname, "dist/demo", "index.html"));
 });
 // Start the app by listening on the default Heroku port
 const server = app.listen(process.env.PORT || 810);
-const gun = Gun({ web: server });
+// const gun = Gun({ web: server });
