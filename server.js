72cf23222d0bcab018d76bbd7b0b98b34ea9ae79
Norman Reed <norman.s.reed@gmail.com>
2021-04-11 07:12:03 -0400

    try exclude gun

--- server.js
+++ server.js
@@ -2,13 +2,9 @@ const path = require("path");
 const express = require("express");
 const Gun = require("gun");
 const app = express();
-app.use(Gun.serve);
 app.use(express.static(__dirname + "/dist/demo"));
-
-app.get("/*", function (req, res) {
-  // console.log({ req, res });
+app.use(Gun.serve);
+app.get("/app/*", function (req, res) {
   res.sendFile(path.join(__dirname, "dist/demo", "index.html"));
 });
-// Start the app by listening on the default Heroku port
-const server = app.listen(process.env.PORT || 80);
-// const gun = Gun({ web: server });
+app.listen(process.env.PORT || 80);
