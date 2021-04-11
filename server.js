eecb0b540a7c6f70d8b4c8578d31f6f22ae2ab0a
Norman Reed <norman.s.reed@gmail.com>
2021-04-11 02:12:39 -0400

    tweak server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const app = express();
 app.use(express.static(__dirname + "/dist/demo"));
 app.get("/*", function (req, res) {
+  // console.log({ req, res });
   res.sendFile(path.join(__dirname, "dist/demo", "index.html"));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 80);
