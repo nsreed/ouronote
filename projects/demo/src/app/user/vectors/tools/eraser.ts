58fed5b92d382ce4d9348d3b8cb2d36c8253a668
Norman Reed <norman.s.reed@gmail.com>
2021-04-22 01:02:13 -0400

    fix eraser hit testing
    now uses a path instead of point-by-point hit tests

--- projects/demo/src/app/user/vectors/tools/eraser.ts
+++ projects/demo/src/app/user/vectors/tools/eraser.ts
@@ -2,15 +2,26 @@ import { VectorTool } from '../paper-tool';
 import { Path, Point, Style, ToolEvent, Project } from 'paper';
 import * as paper from 'paper';
 export class EraserTool extends VectorTool {
-  path?: typeof Path | null;
+  path?: paper.Path | null;
   name = 'eraser';
-  setup() {
-    this.drag.subscribe((e: paper.ToolEvent) => {
-      // console.log('eraser drag');
-      const hits = this.project.hitTestAll(e.point as any);
-      hits.forEach((hitResult: paper.HitResult) => hitResult.item.remove());
-      // console.log('would erase', hits);
+
+  dragSub = this.drag.subscribe((e: paper.ToolEvent) => {
+    // console.log('eraser drag');
+    const prev = (this.scope.settings as any).insertItems;
+    (this.scope.settings as any).insertItems = false;
+    if (!this.path) {
+      this.path = new paper.Path([e.downPoint]);
+      this.path.data.ignored = true;
+    }
+    this.path.add(e.point);
+    const intersects = this.project.getItems({
+      match: (i: paper.Item) =>
+        i !== this.path && !i.data.ignored && i.intersects(this.path as any),
     });
-    this.up.subscribe((e) => (this.path = null));
-  }
+    // TODO skip removing items that were added since the beginning of this drag
+    intersects.forEach((i) => i.remove());
+    (this.scope.settings as any).insertItems = prev;
+  });
+
+  upSub = this.up.subscribe((e) => (this.path = null));
 }
