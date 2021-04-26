b8880ed77db37065d114e2528e71055ce20f7de6
Norman Reed <norman.s.reed@gmail.com>
2021-04-26 19:09:34 -0400

    fix build

--- projects/ng-gun/src/lib/gun-resolver.service.ts
+++ projects/ng-gun/src/lib/gun-resolver.service.ts
@@ -6,18 +6,17 @@ import {
 } from '@angular/router';
 import { Observable } from 'rxjs';
 import { NgGunService } from './ng-gun.service';
-import { UserService } from '../../../demo/src/app/user/user.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class GunResolverService<T> implements Resolve<T> {
-  constructor(private ngGun: NgGunService, private userService: UserService) {}
+  constructor(private ngGun: NgGunService) {}
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<any> {
     const soul = route.params.soul;
-    return this.userService.user.root.get(soul).once();
+    return this.ngGun.auth().root.get(soul).once();
   }
 }
