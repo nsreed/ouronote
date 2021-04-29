653c7a67714212518301dc7adf70a93b9aaaf469
Norman Reed <norman.s.reed@gmail.com>
2021-04-28 20:49:20 -0400

    add buildToolEvent

--- projects/demo/src/app/user/vectors/functions/tool-functions.ts
+++ projects/demo/src/app/user/vectors/functions/tool-functions.ts
@@ -1,4 +1,5 @@
 import { CAPABILITIES } from '../../../system.service';
+import * as paper from 'paper';
 export const byEventType = (type: string) => (event: any) =>
   event.event.pointerType === type;
 export const isMouse = (event: any) => event.event.type.startsWith('mouse');
@@ -14,3 +15,8 @@ export const isTouch = CAPABILITIES.TOUCH
       e.event.pointerType === 'touch' ||
       e.event.type.startsWith('touch')
   : () => false;
+
+export const buildToolEvent = (source: PointerEvent, view: paper.View) =>
+  ({
+    point: view.viewToProject(new paper.Point(source.offsetX, source.offsetY)),
+  } as paper.ToolEvent);
