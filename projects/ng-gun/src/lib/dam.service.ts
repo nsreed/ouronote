2c80b5cd7edbe0d14c00db03c62567a85ffa2a1d
Norman Reed <norman.s.reed@gmail.com>
2021-04-28 20:22:09 -0400

    add DAM service

---
+++ projects/ng-gun/src/lib/dam.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { NgGunService } from './ng-gun.service';
+import { IGunMesh } from './types/gun-mesh';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DamService {
+  mesh: IGunMesh = this.ngGun.gun.back('opt.mesh' as any) as any;
+  constructor(private ngGun: NgGunService) {}
+  disconnect(id: string) {
+    this.mesh.bye(id);
+  }
+}
