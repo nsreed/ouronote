6a636695bd0223c0617103ae0f4259774b1162e2
Norman Reed <norman@normanreed.com>
2021-11-29 14:44:51 -0500

    fix spastic panning

--- projects/demo/src/app/user/vectors/tools/pan.ts
+++ projects/demo/src/app/user/vectors/tools/pan.ts
@@ -2,10 +2,18 @@ import { VectorTool } from './paper-tool';
 import { filter } from 'rxjs/operators';
 import { isMouse, isTouch } from '../functions/tool-functions';
 export class PanTool extends VectorTool {
+  lastDelta?: null | paper.Point;
+  downSub = this.down.subscribe(() => {
+    this.lastDelta = null;
+  });
   dragSub = this.drag
     .pipe(filter((e) => e.delta.length > 0.2))
     .subscribe((e) => {
-      (this.project.view as any).scrollBy((e.delta as any).multiply(-1));
+      const actualDelta = this.lastDelta
+        ? this.lastDelta.subtract(e.delta)
+        : e.delta;
+      (this.project.view as any).scrollBy((actualDelta as any).multiply(1));
+      this.lastDelta = actualDelta;
     });
   filterEvent(event: any) {
     return isMouse(event) || isTouch(event);
