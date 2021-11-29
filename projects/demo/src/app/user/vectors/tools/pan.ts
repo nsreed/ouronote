79fdffbaf09ec58d37a54bbd156dd3f3733339da
Norman Reed <norman@normanreed.com>
2021-11-29 16:34:46 -0500

    give move & pan tool names

--- projects/demo/src/app/user/vectors/tools/pan.ts
+++ projects/demo/src/app/user/vectors/tools/pan.ts
@@ -2,12 +2,13 @@ import { VectorTool } from './paper-tool';
 import { filter } from 'rxjs/operators';
 import { isMouse, isTouch } from '../functions/tool-functions';
 export class PanTool extends VectorTool {
+  name = 'pan';
   lastDelta?: null | paper.Point;
   downSub = this.down.subscribe(() => {
     this.lastDelta = null;
   });
   dragSub = this.drag
-    .pipe(filter((e) => e.delta.length > 0.2))
+    // .pipe(filter((e) => e.delta.length > 0.2))
     .subscribe((e) => {
       const actualDelta = this.lastDelta
         ? this.lastDelta.subtract(e.delta)
