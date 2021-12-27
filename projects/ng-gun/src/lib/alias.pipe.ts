768b3363fc63287297a707b009bffc05e1d9b326
Norman Reed <norman@normanreed.com>
2021-12-27 15:02:39 -0500

    add logging and value checks to owner and alias pipes because bad values may be passed in

--- projects/ng-gun/src/lib/alias.pipe.ts
+++ projects/ng-gun/src/lib/alias.pipe.ts
@@ -3,23 +3,44 @@ import { ChangeDetectorRef, Pipe, PipeTransform } from '@angular/core';
 import { NgGunService } from './ng-gun.service';
 import { map, take, shareReplay } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { LogService } from '../../../log/src/lib/log.service';
 
 @Pipe({
   name: 'alias',
 })
 export class AliasPipe extends AsyncPipe implements PipeTransform {
-  constructor(private ngGun: NgGunService, _ref: ChangeDetectorRef) {
+  constructor(
+    private ngGun: NgGunService,
+    _ref: ChangeDetectorRef,
+    private logger: LogService
+  ) {
     super(_ref);
+    logger.name = 'alias.pipe';
   }
   transform(value: any, ...args: any[]): any {
     if (value === '*') {
       return of(value);
     }
+    if (typeof value !== 'string') {
+      this.logger.error('alias pipe got non-string input: %o', value);
+      return of('UNKNOWN USER');
+    }
     return this.ngGun
       .get(`~${value.replace('~', '')}`)
       .on()
       .pipe(
-        map((v: any) => v.alias || value),
+        map((v: any) => {
+          if (typeof v.alias === 'string') {
+            return v.alias;
+          } else {
+            this.logger.warn(
+              'could not find string alias. Found %o for %s',
+              v.alias,
+              value
+            );
+            return value;
+          }
+        }),
         shareReplay(1)
       );
   }
--- projects/ng-gun/src/lib/owner.pipe.ts
+++ projects/ng-gun/src/lib/owner.pipe.ts
@@ -3,17 +3,30 @@ import { AsyncPipe } from '@angular/common';
 import { NgGunService } from './ng-gun.service';
 import { map, switchMap, shareReplay } from 'rxjs/operators';
 import { of, Observable } from 'rxjs';
+import { LogService } from '../../../log/src/lib/log.service';
+import { GunAuthChain, GunChain } from './classes/GunChain';
 
 @Pipe({
   name: 'owner',
 })
 export class OwnerPipe extends AsyncPipe implements PipeTransform {
-  constructor(private ngGun: NgGunService, _ref: ChangeDetectorRef) {
+  constructor(
+    private ngGun: NgGunService,
+    _ref: ChangeDetectorRef,
+    private logger: LogService
+  ) {
     super(_ref);
   }
 
   transform(value: any, ...args: unknown[]): any {
-    // value is a GunChain
+    // value must be a GunChain
+    if (!value.owner) {
+      this.logger.error(
+        'owner pipe input does not contain an owner! got: ',
+        value
+      );
+      return null;
+    }
 
     const ownerKVP$ = this.ngGun.get(value.owner['#']).on();
     const ownerAlias$ = ownerKVP$.pipe(
