03183c9bad3e99ad3d037c20b3f858ec0e395702
Norman Reed <norman@normanreed.com>
2021-12-26 19:22:54 -0500

    notify user when session problem is detected

--- projects/demo/src/app/user/user.component.ts
+++ projects/demo/src/app/user/user.component.ts
@@ -3,6 +3,11 @@ import { UserService } from './user.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { CertificatesComponent } from '../components/certificates/certificates.component';
+import { timer } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { LogService } from '../../../../log/src/lib/log.service';
+import { NgGunService } from '../../../../ng-gun/src/lib/ng-gun.service';
+import { ConfirmComponent } from '../components/confirm/confirm.component';
 
 @Component({
   selector: 'app-user',
@@ -10,13 +15,31 @@ import { CertificatesComponent } from '../components/certificates/certificates.c
   styleUrls: ['./user.component.css'],
 })
 export class UserComponent implements OnInit {
+  authPoll$ = timer(1000, 1000).pipe(map((t) => this.ngGun.auth().alias));
+  hasConfirmDialog = false;
+  lastAlias: string =
+    'LKDHFS:LSKDHFDSYFNBWEOHFPOSIDHGS:DLKN:LSDKFHOIFYSODHFGS:DGHSDYLIEHTLKHLGSHDGHSDGF:I SDYUFISYDFNBSDOIFNPSDOIFNUDSPOGUNPSDOGSDPOGSHEPGOIEMGESIOGHMSEGPOSEHG';
   constructor(
     private userService: UserService,
     router: Router,
     route: ActivatedRoute,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private logger: LogService,
+    private ngGun: NgGunService
   ) {
     // console.log('user route', route.routeConfig?.children);
+    this.authPoll$.subscribe((alias) => {
+      if (
+        // Math.random() < 0.5 ||
+        this.lastAlias !==
+          'LKDHFS:LSKDHFDSYFNBWEOHFPOSIDHGS:DLKN:LSDKFHOIFYSODHFGS:DGHSDYLIEHTLKHLGSHDGHSDGF:I SDYUFISYDFNBSDOIFNPSDOIFNUDSPOGUNPSDOGSDPOGSHEPGOIEMGESIOGHMSEGPOSEHG' &&
+        this.lastAlias !== undefined &&
+        this.lastAlias !== alias
+      ) {
+        this.onAuthChange();
+      }
+      this.lastAlias = alias;
+    });
   }
 
   ngOnInit(): void {
@@ -25,5 +48,29 @@ export class UserComponent implements OnInit {
 
   openCertificates() {
     this.dialog.open(CertificatesComponent, { height: '90%', width: '90%' });
+  }
+
+  onAuthChange() {
+    if (!this.hasConfirmDialog) {
+      this.hasConfirmDialog = true;
+      this.dialog
+        .open(ConfirmComponent, {
+          data: {
+            title: 'ono Oh No!',
+            prompt:
+              'ouronote has encountered a problem and needs to refresh. You will need to log in again.',
+            options: [
+              {
+                text: 'OK',
+              },
+            ],
+          },
+        })
+        .afterClosed()
+        .subscribe((result) => {
+          this.userService.user.logout();
+          this.hasConfirmDialog = false;
+        });
+    }
   }
 }
