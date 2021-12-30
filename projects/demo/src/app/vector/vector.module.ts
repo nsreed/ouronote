7d6d6ed12063edc2709e92500818364a425547cb
Norman Reed <norman@normanreed.com>
2021-12-29 23:11:29 -0500

    add missing imports for vector module

--- projects/demo/src/app/vector/vector.module.ts
+++ projects/demo/src/app/vector/vector.module.ts
@@ -12,6 +12,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { NgGunModule } from '../../../../ng-gun/src/lib/ng-gun.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @NgModule({
   declarations: [
@@ -27,7 +30,10 @@ import { NgGunModule } from '../../../../ng-gun/src/lib/ng-gun.module';
     MatButtonModule,
     VectorRoutingModule,
     ReactiveFormsModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
     MatInputModule,
+    MatIconModule,
     NgGunModule,
   ],
   exports: [PaperDirective, PaperEditDirective, VectorPreviewComponent],
