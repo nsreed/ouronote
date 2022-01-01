0601624f00cdddc561890f38224bde582b4c81bc
Norman Reed <norman@normanreed.com>
2021-12-31 22:25:36 -0500

    update look & feel of vector settings/invite requests

--- projects/demo/src/app/user/vectors/components/settings-dialog/settings-dialog.component.ts
+++ projects/demo/src/app/user/vectors/components/settings-dialog/settings-dialog.component.ts
@@ -3,6 +3,8 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { VectorGraph } from '../../../VectorGraph';
 import { VectorService } from '../../vector.service';
 import { UserService } from '../../../user.service';
+import { shareReplay, map } from 'rxjs/operators';
+import { GunChain } from '../../../../../../../ng-gun/src/lib/classes/GunChain';
 
 export interface VectorSettingsData {
   mode: 'general' | 'people';
@@ -15,9 +17,20 @@ export interface VectorSettingsData {
 })
 export class SettingsDialogComponent implements OnInit {
   vectors = this.vectorService.vectors;
-  vector = this.vectors.get(this.data.vectorPub as never);
+  vector: GunChain<VectorGraph> = this.vectors.get(
+    this.data.vectorPub as never
+  ) as any;
 
   vector$ = this.vector.on();
+  requests$ = this.vector
+    .get('inviteRequests')
+    .open()
+    .pipe(
+      // TODO make sure this open() call isn't making the program die
+      map((requests: any) => Object.keys(requests).filter((k) => requests[k])),
+      shareReplay(1)
+    );
+  requestCount$ = this.requests$.pipe(map((r) => r.length));
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public readonly data: VectorSettingsData,
