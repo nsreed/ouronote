d7d3b053834747001ba9abf8dfafd1b0f97f5e6e
Norman Reed <norman@normanreed.com>
2022-02-01 02:38:45 -0500

    fix getRandomName() when no blacklist is provided

--- projects/demo/src/app/services/name-randomizer.service.ts
+++ projects/demo/src/app/services/name-randomizer.service.ts
@@ -11,28 +11,14 @@ export class NameRandomizerService {
   blacklist$ = this.http
     .get('/assets/dictionary/blacklist.json')
     .pipe(shareReplay(1));
-  adjectives$ = this.http
-    .get('/assets/dictionary/adjectives.json')
-    .pipe(shareReplay(1));
-
-  adjective$ = this.blacklist$.pipe(
-    switchMap((b: any) =>
-      this.adjectives$.pipe(filter((a) => !b || !b.includes(a)))
-    ),
-    map((n: any) => n[Math.floor(Math.random() * n.length)])
-  );
-  nouns$ = this.http.get('/assets/dictionary/nouns.json').pipe(shareReplay(1));
-  noun$ = this.nouns$.pipe(
-    switchMap((b: any) =>
-      this.nouns$.pipe(filter((n) => !b || !b.includes(n)))
-    ),
-    map((n: any) => n[Math.floor(Math.random() * n.length)])
-  );
 
   constructor(private http: HttpClient) {}
 
   async getRandomName() {
-    const bl = (await this.blacklist$.toPromise()) as any[];
+    let bl: any = [];
+    try {
+      bl = (await this.blacklist$.toPromise()) as any[];
+    } catch (e: any) {}
     const appAdj = adjectives.filter((a) => !bl.includes(a));
     const appNoun = nouns.filter((n) => !bl.includes(n));
     const ra = appAdj[Math.floor(Math.random() * appAdj.length)];
