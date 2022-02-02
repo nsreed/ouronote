3eb698188bde3d619cb55124e9cba0c86f97ba90
Norman Reed <norman@normanreed.com>
2022-02-02 17:14:28 -0500

    add license tab to vector settings

--- projects/demo/src/app/user/vectors/components/settings-dialog/settings-dialog.component.ts
+++ projects/demo/src/app/user/vectors/components/settings-dialog/settings-dialog.component.ts
@@ -3,8 +3,10 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { VectorGraph } from '../../../VectorGraph';
 import { VectorService } from '../../vector.service';
 import { UserService } from '../../../user.service';
-import { shareReplay, map } from 'rxjs/operators';
+import { shareReplay, map, switchMap } from 'rxjs/operators';
 import { GunChain } from '../../../../../../../ng-gun/src/lib/classes/GunChain';
+import { Validators, FormBuilder } from '@angular/forms';
+import { LICENSES } from '../../../../LICENSES';
 
 export interface VectorSettingsData {
   mode: 'general' | 'people';
@@ -16,6 +18,17 @@ export interface VectorSettingsData {
   styleUrls: ['./settings-dialog.component.scss'],
 })
 export class SettingsDialogComponent implements OnInit {
+  form = this.fb.group({
+    title: [null, Validators.required],
+    license: null,
+    customLicense: this.fb.group({
+      name: null,
+      text: `Copyright © ${new Date().getFullYear()} ${
+        this.userService.user.alias
+      } ALL RIGHTS RESERVED`,
+    }),
+  });
+
   vectors = this.vectorService.vectors;
   vector: GunChain<VectorGraph> = this.vectors.get(
     this.data.vectorPub as never
@@ -36,8 +49,31 @@ export class SettingsDialogComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public readonly data: VectorSettingsData,
     private vectorService: VectorService,
     private userService: UserService,
-    public dialogRef: MatDialogRef<any>
-  ) {}
+    public dialogRef: MatDialogRef<any>,
+    private fb: FormBuilder
+  ) {
+    this.vector
+      .get('license')
+      .open()
+      .subscribe((l: any) => {
+        const license = Object.values(LICENSES).find(
+          (ll: any) => ll.type === l.type
+        );
+        this.form.controls.license.setValue(license);
+      });
+  }
 
   ngOnInit(): void {}
+
+  onDoneClick() {
+    if (this.form.dirty) {
+      const formValue = this.form.value;
+      const license =
+        formValue.license === 'custom'
+          ? formValue.customLicense
+          : formValue.license;
+      this.vector.get('license').put(license);
+    }
+    this.dialogRef.close();
+  }
 }
