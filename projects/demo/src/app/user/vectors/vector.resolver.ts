7253541bee3cf62936b0fdd1edabf4c3efd2d49b
Norman Reed <norman@normanreed.com>
2022-03-11 19:40:06 -0500

    fix vector editor crashing when navigating to nonexistent vector

--- projects/demo/src/app/user/vectors/vector.resolver.ts
+++ projects/demo/src/app/user/vectors/vector.resolver.ts
@@ -5,10 +5,20 @@ import {
   RouterStateSnapshot,
   ActivatedRouteSnapshot,
 } from '@angular/router';
-import { Observable, of } from 'rxjs';
-import { map, switchMap, tap } from 'rxjs/operators';
+import { EMPTY, Observable, of, from, merge } from 'rxjs';
+import {
+  map,
+  switchMap,
+  tap,
+  filter,
+  mergeAll,
+  take,
+  mergeMap,
+  combineAll,
+} from 'rxjs/operators';
 import { VectorService } from './vector.service';
 import { NgGunService } from '../../../../../ng-gun/src/lib/ng-gun.service';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -16,22 +26,40 @@ import { NgGunService } from '../../../../../ng-gun/src/lib/ng-gun.service';
 export class VectorResolver implements Resolve<boolean> {
   constructor(
     private vectorService: VectorService,
-    private ngGun: NgGunService
+    private ngGun: NgGunService,
+    private router: Router
   ) {}
   resolve(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ): Observable<boolean> {
-    return this.vectorService.vectors
-      .get(route.params.soul)
-      .once()
-      .pipe(
-        // tap((vector: any) => console.log('got vector', vector)),
-        switchMap((vector: any) =>
-          vector ? of(vector) : this.ngGun.get(route.params.soul).once()
-        ),
-        // tap((vector: any) => console.log('got vector', vector)),
-        map((post: any) => post._)
-      );
+  ): Observable<any> {
+    const vectorNode = this.vectorService.vectors.get(route.params.soul);
+    const not$ = vectorNode.not().pipe(
+      tap((v) => {
+        console.error('could not find vector');
+        throw new Error('vector not found');
+      })
+    );
+    const exists$ = vectorNode.once().pipe(
+      // tap((vector: any) => console.log('got vector', vector)),
+      filter((vector: any) => vector),
+      switchMap((vector: any) =>
+        vector ? of(vector) : this.ngGun.get(route.params.soul).once()
+      ),
+      // tap((vector: any) => console.log('got vector', vector)),
+      map((vector: any) => vector._)
+    );
+    return from([not$, exists$]).pipe(
+      mergeAll(),
+      catchError((err) => {
+        this.router.navigateByUrl(
+          (route as any)._urlSegment.segments[0].path === 'user'
+            ? '/user/vectors'
+            : ''
+        );
+        return EMPTY;
+      }),
+      take(1)
+    );
   }
 }
