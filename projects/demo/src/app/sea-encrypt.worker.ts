9cb00a2dde41d2ae37d2b8eeba8e2d06715301a1
Norman Reed <norman@normanreed.com>
2022-06-15 11:31:46 -0400

    remove unused worker

--- projects/demo/src/app/sea-encrypt.worker.ts
+++
@@ -1,7 +0,0 @@
-/// <reference lib="webworker" />
-import * as Gun from 'gun';
-
-addEventListener('message', ({ data }) => {
-  const response = `worker response to ${data}`;
-  postMessage(response);
-});
