1025e78ceeda28244878443cb3eeaec5ec29d210
Norman Reed <norman@normanreed.com>
2022-07-19 10:45:04 -0400

    version.js: use a hash in addition to the verion number in package.json

--- version.js
+++ version.js
@@ -1,33 +1,44 @@
 const { version } = require("./package.json");
 const { resolve, relative } = require("path");
 const { writeFileSync } = require("fs-extra");
+const { hashElement } = require('folder-hash');
+// const crypto = require('crypto');
+// const sha1 = crypto.createHash('sha1');
 
-const gitInfo = {};
+const opt = {
+  encoding: 'hex',
+  folders: { exclude: ['.*', 'node_modules', 'test_coverage', 'coverage', 'typings', 'radata'] },
+  files: { include: ['**/*.js', '**/*.ts', '**/*.json'] }
+};
 
-gitInfo.version = version;
+hashElement('./dist', opt).then(dirHash => {
+  // sha1.update(dirHash);
+  // const hash = sha1.digest('hex');
+  // console.log(hash);
+  const hash = dirHash.hash;
 
-const file = resolve(
-  __dirname,
-  "projects",
-  "demo",
-  "src",
-  "environments",
-  "version.ts"
-);
-writeFileSync(
-  file,
-  `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
-/* tslint:disable */
-export const VERSION = ${JSON.stringify(gitInfo, null, 4)};
-/* tslint:enable */
-`,
-  { encoding: "utf-8" }
-);
-
-// tslint:disable-next-line: no-console
-console.log(
-  `Wrote version info ${JSON.stringify(gitInfo)} to ${relative(
-    resolve(__dirname, ".."),
-    file
-  )}`
-);
+  const file = resolve(
+    __dirname,
+    "projects",
+    "demo",
+    "src",
+    "environments",
+    "version.ts"
+  );
+  writeFileSync(
+    file,
+    `// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+  /* tslint:disable */
+  export const VERSION = ${JSON.stringify({ version, hash }, null, 4)};
+  /* tslint:enable */
+  `,
+    { encoding: "utf-8" }
+  );
+  //tslint:disable-next-line:no-console
+  console.log(
+    `Wrote version info ${JSON.stringify({ version, hash })} to ${relative(
+      resolve(__dirname, ".."),
+      file
+    )}`
+  );
+}).catch(error => console.error('hash failed', error));
